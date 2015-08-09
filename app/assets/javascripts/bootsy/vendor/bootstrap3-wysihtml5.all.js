1afa26c4319125bb01598a0f65aa36a1106905c1
yunwei <stillwyw@gmail.com>
2015-08-10 07:07:00 +0800

    allowing javascript and style tag in pasted html.

--- app/assets/javascripts/bootsy/vendor/bootstrap3-wysihtml5.all.js
+++ app/assets/javascripts/bootsy/vendor/bootstrap3-wysihtml5.all.js
@@ -13240,7 +13240,7 @@ wysihtml5.views.View = Base.extend(
     handleTabKey:         true,
     // Object which includes parser rules to apply when html gets cleaned
     // See parser_rules/*.js for examples
-    parserRules:          { tags: { br: {}, span: {}, div: {}, p: {} }, classes: {} },
+    parserRules:          { tags: { br: {}, span: {}, div: {}, p: {}, style: {}, script: {} }, classes: {} },
     // Object which includes parser when the user inserts content via copy & paste. If null parserRules will be used instead
     pasteParserRulesets: null,
     // Parser method to use when the user inserts content
